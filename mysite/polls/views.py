b504eee7ec25a23ed516897a5d8c55f53881c7a2
Jo Chasinga <jo.chasinga@gmail.com>
2014-03-26 02:24:19 +0700

    add shortcut render() to index view

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,15 +1,13 @@
 # Create your views here.
 from django.http import HttpResponse
-from django.template import RequestContext, loader
+# from django.template import RequestContext, loader
+from django.shortcuts import render
 from polls.models import Poll
 
 def index(req):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(req, {
-            'latest_poll_list': latest_poll_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_poll_list': latest_poll_list}
+    return render(req, 'polls/index.html', context)
 
 def detail(req, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
